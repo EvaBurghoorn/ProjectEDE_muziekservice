fd47295ed226495b8a45e84a9ccb80ec0c798857
r0889623 <r0889623@student.thomasmore.be>
2023-09-29 08:35:37 +0200

    aanmaken projecten

---
+++ music-podcast-service/src/main/java/fact/it/musicpodcastservice/MusicPodcastServiceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.musicpodcastservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPodcastServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPodcastServiceApplication.class, args);
+	}
+
+}
