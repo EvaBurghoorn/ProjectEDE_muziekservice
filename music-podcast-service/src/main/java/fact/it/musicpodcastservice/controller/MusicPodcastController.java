23b4b80c93c1772b2dc58b7b35d9d048e5c0e852
r0889623 <r0889623@student.thomasmore.be>
2023-12-23 11:01:56 +0100

    aanpassingen

--- music-podcast-service/src/main/java/fact/it/musicpodcastservice/controller/MusicPodcastController.java
+++ music-podcast-service/src/main/java/fact/it/musicpodcastservice/controller/MusicPodcastController.java
@@ -53,21 +53,20 @@ public class MusicPodcastController {
     }
 
 
-//    @GetMapping("/ratings/{username}")
-//    public ResponseEntity<List<MusicPodcastResponse>> getAllMusicPodcastsWithRatingLikedPerUser(@PathVariable String username) {
-//        MusicPodcastRequest musicPodcastRequest = new MusicPodcastRequest();
-//        RatingResponse ratingResponse = new RatingResponse();
-//        ratingResponse.setUsername(username);
-//
-//        List<MusicPodcastResponse> musicPodcastResponses = musicPodcastService.getAllMusicPodcastsWithRatingLikedPerUser(musicPodcastRequest, ratingResponse);
-//        return new ResponseEntity<>(musicPodcastResponses, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/ratings/{username}/{uniqueIdentifier}")
-//    public ResponseEntity<MusicPodcast> getMusicPodcastPerUserByUsername(@PathVariable String username, @PathVariable String uniqueIdentifier) {
-//        MusicPodcast musicPodcast = musicPodcastService.getMusicPodcastPerUser(username, uniqueIdentifier);
-//        return new ResponseEntity<>(musicPodcast, HttpStatus.OK);
-//    }
+    @GetMapping("/ratings/{username}")
+    public ResponseEntity<List<MusicPodcastResponse>> getAllMusicPodcastsWithRatingLikedPerUser(@PathVariable String username) {
+        RatingResponse ratingResponse = new RatingResponse();
+        ratingResponse.setUsername(username);
+
+        List<MusicPodcastResponse> musicPodcastResponses = musicPodcastService.getAllMusicPodcastsWithRatingLikedPerUser(ratingResponse);
+        return new ResponseEntity<>(musicPodcastResponses, HttpStatus.OK);
+    }
+
+    @GetMapping("/ratings/{username}/{uniqueIdentifier}")
+    public ResponseEntity<MusicPodcast> getMusicPodcastPerUserByUsername(@PathVariable String username, @PathVariable String uniqueIdentifier) {
+        MusicPodcast musicPodcast = musicPodcastService.getMusicPodcastPerUser(username, uniqueIdentifier);
+        return new ResponseEntity<>(musicPodcast, HttpStatus.OK);
+    }
 
 
 }
